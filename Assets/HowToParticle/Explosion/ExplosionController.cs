3c950a4bea842858dc06fd64a372c3876169584a
Tom Mensink <tom@mensink.com>
2017-07-26 02:09:31 +0200

    Inital commit

---
+++ Assets/HowToParticle/Explosion/ExplosionController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionController : MonoBehaviour {
+    [Tooltip("Object to destroy at explosion")]
+    public GameObject ObjectToDestroyAtExplosion;
+
+    [Tooltip("Explosion")]
+    public GameObject ExplosionObject;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+    public void Explode()
+    {
+        if (ExplosionObject != null)
+        {
+            //var exp = ExplosionObject.GetComponent<ParticleSystem>();
+            //exp.Play();
+            //Destroy(ExplosionObject, exp.main.duration);
+
+            ExplosionObject.SetActive(true);
+            if (ObjectToDestroyAtExplosion != null) { Destroy(ObjectToDestroyAtExplosion); }
+        }
+    }
+}
---
+++ Assets/HowToParticle/GameController - Obsolete/GameController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.Events;
+using UnityEngine.VR.WSA.Input;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+
+public class GameController : MonoBehaviour, IInputClickHandler
+{
+    [Tooltip("On click event")]
+    public UnityEvent OnClickEvent;
+
+    private GestureRecognizer recognizer;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Start GameController", gameObject);
+
+        recognizer = new GestureRecognizer();
+        recognizer.SetRecognizableGestures(GestureSettings.Tap);
+        recognizer.TappedEvent += MyTapEventHandler;
+        recognizer.StartCapturingGestures();
+    }
+
+    // Update is called once per frame
+    void Update () {
+/*
+        if (Input.GetButton("Fire1")) // Ctrl key or Left mouse
+        {
+            if (OnClickEvent != null) { OnClickEvent.Invoke(); }
+        }
+*/
+    }
+
+    private void MyTapEventHandler(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        Debug.Log("MyTapEventHandler", gameObject);
+        if (OnClickEvent != null) { OnClickEvent.Invoke(); }
+    }
+
+    public virtual void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("OnInputClicked", gameObject);
+        if (OnClickEvent != null) { OnClickEvent.Invoke(); }
+    }
+
+}
